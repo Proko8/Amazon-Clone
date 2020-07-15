14f000961cfc65ed6652958ddadd06ebaba1ba02
Proko8 <paulprokopy@Pauls-MacBook-Air.local>
2020-07-14 23:47:11 -0400

    14 July Final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Header from "./Header"
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Switch>
-        <Route path="/checkout">
-          <h1>Checkout Page</h1>
-        </Route>
-        <Route path="/login">
-          <h1>Login Page</h1>
-        </Route>
-        <Route path="/">
-          <Header />
-          <h1>Home Page</h1>
-        </Route>
-      </Switch>
-    </div>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>Checkout Page</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+
+            <Home></Home>
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Product from './Product'
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <img className="home__image"
+            src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" 
+            alt=""/>
+
+        {/* Product, id,title, rating, image */}
+        {/*  Product */}
+        <Product id='12321341'
+        title='The Lean Startup: How Constant Inovation Creates Radically Sucessful Businesses Paperback'
+        price={11.96}
+        rating={5}
+        image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg'
+        />
+        </div>
+    )
+}
+
+export default Home
